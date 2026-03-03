93894f1968d1bcae4215962fc154459fa9c3994b
xxcosita3czxx <kokosove18@gmail.com>
2026-03-03 23:45:56 +0100

    putting back logic for instruction set checking

--- AVX2Patch/AVX2Patch.c
+++ AVX2Patch/AVX2Patch.c
@@ -40,10 +40,69 @@ typedef void (*trap_handler_t)(x86_saved_state_t *);
 #define DBG_LOG(fmt, ...) do {} while(0)
 #endif
 
+static bool has_sse = false;
+static bool has_sse2 = false;
+static bool has_sse3 = false;
+static bool has_ssse3 = false;
+static bool has_sse41 = false;
+static bool has_sse42 = false;
+static bool has_avx = false;
+static bool has_fma = false;
+static bool has_avx2 = false;
+
 static trap_handler_t *idt_table = NULL;
 static trap_handler_t original_ud = NULL;
 
 extern long SymbolLookup(const char *symbol_name);
+
+
+static void check_instruction_sets(void)
+{
+    os_log(OS_LOG_DEFAULT,"[AVX2Patch] Entering check_instruction_sets()\n");
+    unsigned int eax, ebx, ecx, edx;
+
+    // CPUID leaf 1: basic features
+    eax = 1;
+    __asm__ volatile (
+        "cpuid"
+        : "+a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+        :
+        :
+    );
+    has_sse   = (edx & (1 << 25)) != 0;
+    has_sse2  = (edx & (1 << 26)) != 0;
+    has_sse3  = (ecx & (1 << 0))  != 0;
+    has_ssse3 = (ecx & (1 << 9))  != 0;
+    has_sse41 = (ecx & (1 << 19)) != 0;
+    has_sse42 = (ecx & (1 << 20)) != 0;
+    has_avx   = (ecx & (1 << 28)) != 0;
+    has_fma   = (ecx & (1 << 12)) != 0;
+
+    DBG_LOG("[AVX2Patch] After CPUID leaf 1\n");
+
+    // CPUID leaf 7, subleaf 0: extended features (AVX2)
+    eax = 7; ecx = 0;
+    __asm__ volatile (
+        "cpuid"
+        : "+a" (eax), "=b" (ebx), "+c" (ecx), "=d" (edx)
+        :
+        :
+    );
+    has_avx2 = (ebx & (1 << 5)) != 0;
+
+    DBG_LOG("[AVX2Patch] After CPUID leaf 7\n");
+
+    DBG_LOG("[AVX2Patch] SSE: %s\n", has_sse ? "Supported" : "Not Supported");
+    DBG_LOG("[AVX2Patch] SSE2: %s\n", has_sse2 ? "Supported" : "Not Supported");
+    DBG_LOG("[AVX2Patch] SSE3: %s\n", has_sse3 ? "Supported" : "Not Supported");
+    DBG_LOG("[AVX2Patch] SSSE3: %s\n", has_ssse3 ? "Supported" : "Not Supported");
+    DBG_LOG("[AVX2Patch] SSE4.1: %s\n", has_sse41 ? "Supported" : "Not Supported");
+    DBG_LOG("[AVX2Patch] SSE4.2: %s\n", has_sse42 ? "Supported" : "Not Supported");
+    DBG_LOG("[AVX2Patch] AVX: %s\n", has_avx ? "Supported" : "Not Supported");
+    DBG_LOG("[AVX2Patch] FMA: %s\n", has_fma ? "Supported" : "Not Supported");
+    DBG_LOG("[AVX2Patch] AVX2: %s\n", has_avx2 ? "Supported" : "Not Supported");
+}
+
 
 static void my_ud_handler(x86_saved_state_t *state)
 {
