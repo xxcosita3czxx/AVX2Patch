57a7d37cf79e0ce363d9aa02c2a0c203022231d3
xxcosita3czxx <kokosove18@gmail.com>
2026-03-03 23:44:03 +0100

    define something nonexistent in kernel on system fsr

--- AVX2Patch/AVX2Patch.c
+++ AVX2Patch/AVX2Patch.c
@@ -1,11 +1,38 @@
 #include <mach/mach_types.h>
 #include <i386/trap.h>
-#include <i386/thread_status.h>
 #include <os/log.h>
 #include <libkern/libkern.h>
 #include <stdint.h>
 
 #define UD_VECTOR 6
+
+typedef struct {
+    struct {
+        uint64_t rdi;
+        uint64_t rsi;
+        uint64_t rbp;
+        uint64_t rsp;
+        uint64_t rbx;
+        uint64_t rdx;
+        uint64_t rcx;
+        uint64_t rax;
+        uint64_t r8;
+        uint64_t r9;
+        uint64_t r10;
+        uint64_t r11;
+        uint64_t r12;
+        uint64_t r13;
+        uint64_t r14;
+        uint64_t r15;
+        uint64_t rip;
+        uint64_t rflags;
+        uint16_t cs;
+        uint16_t fs;
+        uint16_t gs;
+    } isf;
+} x86_saved_state_t;
+
+typedef void (*trap_handler_t)(x86_saved_state_t *);
 
 #ifdef DEBUG
 #define DBG_LOG(fmt, ...) kprintf(fmt, ##__VA_ARGS__)
